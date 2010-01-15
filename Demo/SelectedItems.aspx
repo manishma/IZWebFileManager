54186dba8bc58260b4093118166b1a017eeff70c
Igor Zelmanovich <igor.israel@gmail.com>
2010-01-15 09:01:53 +0000

    add SelectedItems demo

---
+++ Demo/SelectedItems.aspx
@@ -0,0 +1,21 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPage.master" AutoEventWireup="true"
+	CodeFile="SelectedItems.aspx.cs" Inherits="SelectedItems" %>
+
+<%@ Register TagPrefix="iz" Namespace="IZ.WebFileManager" Assembly="IZ.WebFileManager" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="ContentPlaceHolder1" runat="Server">
+	<div>
+		<iz:FileManager AllowUpload="false" ID="FileManager1" runat="server" Height="400" Width="600">
+			<RootDirectories>
+				<iz:RootDirectory DirectoryPath="~/Files/My Documents" Text="My Documents" />
+			</RootDirectories>
+		</iz:FileManager>
+	</div>
+	<div style="padding:5px;">
+		<asp:Label runat="server" ID="SelectedItemsLog" />
+	</div>
+	<div style="padding:5px;">
+		<asp:Button runat="server" Text="Log current directory" OnClick="LogCurrentDirectory" />
+		<asp:Button runat="server" Text="Log selected items" OnClick="LogSelected" />
+		<asp:FileUpload runat="server" ID="FileUpload1" /><asp:Button runat="server" Text="Upload file" OnClick="Upload"/>
+	</div>
+</asp:Content>
---
+++ Demo/SelectedItems.aspx.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using IZ.WebFileManager;
+
+public partial class SelectedItems : System.Web.UI.Page
+{
+	protected void Page_Load (object sender, EventArgs e) {
+
+	}
+
+	protected void LogSelected (object sender, EventArgs e) {
+		SelectedItemsLog.Text = "";
+
+		if (FileManager1.SelectedItems.Length == 0)
+			SelectedItemsLog.Text = "Please, select any item first.<br />";
+		else
+			foreach (FileManagerItemInfo item in FileManager1.SelectedItems) {
+				SelectedItemsLog.Text += item.VirtualPath + "<br />";
+
+			}
+	}
+
+	protected void LogCurrentDirectory (object sender, EventArgs e) {
+		SelectedItemsLog.Text = "";
+		SelectedItemsLog.Text = FileManager1.CurrentDirectory.VirtualPath + "<br />";
+	}
+
+	protected void Upload(object sender, EventArgs e)
+	{
+		SelectedItemsLog.Text = "";
+		if(FileUpload1.HasFile)
+		{
+			string name = Path.GetFileName(FileUpload1.FileName);
+			string vdir = FileManager1.CurrentDirectory.VirtualPath;
+			string dir = FileManager1.CurrentDirectory.PhysicalPath;
+
+			FileUpload1.SaveAs(Path.Combine(dir, name));
+
+			SelectedItemsLog.Text = String.Format("File '{0}' was uploaded to '{1}' successfuly.<br />", name, vdir);
+		}
+		else
+			SelectedItemsLog.Text = "Please, select file to upload.<br />";
+	}
+}

870a7067e639dfd4207596a96bfa18bd08431244
Igor Zelmanovich <igor.israel@gmail.com>
2010-03-13 15:34:18 +0000

    dispose images properly using 'using'

--- IZWebFileManager/ThumbnailHandler.cs
+++ IZWebFileManager/ThumbnailHandler.cs
@@ -7,25 +7,40 @@ using System.Drawing.Imaging;
 
 namespace IZ.WebFileManager
 {
-	public class ThumbnailHandler : IHttpHandler
-	{
-		public bool IsReusable {
-			get { return true; }
-		}
+    public class ThumbnailHandler : IHttpHandler
+    {
+        public bool IsReusable
+        {
+            get { return true; }
+        }
 
-		public void ProcessRequest (HttpContext context) {
-			context.Response.ContentType = "image/jpeg";
-			int size = 92;
+        public void ProcessRequest(HttpContext context)
+        {
+            context.Response.ContentType = "image/jpeg";
+            int size = 92;
 
-			string vPath = HttpUtility.UrlDecode (context.Request.Url.Query.Substring (1));
-			string path = context.Request.MapPath (vPath);
-			Image img = Image.FromFile (path);
-			if (img.Width > size || img.Height > size) {
-				int tw = img.Width > img.Height ? size : (img.Width * size) / img.Height;
-				int th = img.Width > img.Height ? (img.Height * size) / img.Width : size;
-				img = img.GetThumbnailImage (tw, th, null, IntPtr.Zero);
-			}
-			img.Save (context.Response.OutputStream, ImageFormat.Jpeg);
-		}
-	}
+            string vPath = HttpUtility.UrlDecode(context.Request.Url.Query.Substring(1));
+            string path = context.Request.MapPath(vPath);
+
+            using (var original = Image.FromFile(path))
+            {
+                if (original.Width > size || original.Height > size)
+                {
+                    int tw = original.Width > original.Height ? size : (original.Width * size) / original.Height;
+                    int th = original.Width > original.Height ? (original.Height * size) / original.Width : size;
+
+                    using (var thumb = original.GetThumbnailImage(tw, th, null, IntPtr.Zero))
+                    {
+                        thumb.Save(context.Response.OutputStream, ImageFormat.Jpeg);
+
+                    }
+                }
+                else
+                {
+                    original.Save(context.Response.OutputStream, ImageFormat.Jpeg);
+                }
+            }
+
+        }
+    }
 }

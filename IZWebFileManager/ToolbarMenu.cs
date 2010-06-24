1687ec1fab074cdc71eda0ebdf2ee1c4da70ea55
Igor Zelmanovich <igor.israel@gmail.com>
2010-06-24 12:13:47 +0300

    drop down toolbar's submenu

--- IZWebFileManager/ToolbarMenu.cs
+++ IZWebFileManager/ToolbarMenu.cs
@@ -23,6 +23,27 @@ namespace IZ.WebFileManager
             return toolbarMenuAdapter;
         }
 
+        protected override void OnPreRender(EventArgs e)
+        {
+            base.OnPreRender(e);
+
+            var script =
+@"
+
+function IZWebFileManager_ShowElement(id) {
+    var el = WebForm_GetElementById(id);
+    el.style.display = 'block';
+}
+
+function IZWebFileManager_HideElement(id) {
+    var el = WebForm_GetElementById(id);
+    el.style.display = 'none';
+}
+
+";
+            Page.ClientScript.RegisterClientScriptBlock(typeof(ToolbarMenu), "show_hide_submenu", script, true);
+        }
+
         internal class ToolbarMenuAdapter : MenuAdapter
         {
             private static readonly FieldInfo _control =
@@ -51,11 +72,21 @@ namespace IZ.WebFileManager
             }
             protected override void RenderItem(HtmlTextWriter writer, MenuItem item, int position)
             {
+                var hasChildren = item.ChildItems.Count > 0;
+                var itemControl = ((MenuItemTemplateContainer) _container.GetValue(item, null));
+                var submenuClientId = itemControl.ClientID + "_s";
+
+                if(hasChildren)
+                {
+                    writer.AddAttribute("onmouseover", "IZWebFileManager_ShowElement('" + submenuClientId + "')");
+                    writer.AddAttribute("onmouseout", "IZWebFileManager_HideElement('" + submenuClientId + "')");
+                }
                 writer.RenderBeginTag(HtmlTextWriterTag.Td);
 
-                ((MenuItemTemplateContainer) _container.GetValue(item, null)).RenderControl(writer);
 
-                if(item.ChildItems.Count> 0)
+                itemControl.RenderControl(writer);
+
+                if(hasChildren)
                 {
                     writer.AddStyleAttribute(HtmlTextWriterStyle.Display, "none");
                     writer.AddStyleAttribute(HtmlTextWriterStyle.Position, "absolute");
@@ -63,15 +94,23 @@ namespace IZ.WebFileManager
                     writer.AddAttribute(HtmlTextWriterAttribute.Cellspacing, "0");
                     writer.AddAttribute(HtmlTextWriterAttribute.Cellpadding, "0");
                     writer.AddAttribute(HtmlTextWriterAttribute.Border, "0");
+                    writer.AddAttribute(HtmlTextWriterAttribute.Id, submenuClientId);
                     writer.RenderBeginTag(HtmlTextWriterTag.Table);
                     writer.RenderBeginTag(HtmlTextWriterTag.Tr);
                     writer.RenderBeginTag(HtmlTextWriterTag.Td);
 
                     for (int i = 0; i < item.ChildItems.Count; i++)
                     {
+                        Control.DynamicMenuItemStyle.AddAttributesToRender(writer);
+                        writer.AddAttribute("onmouseover", "WebForm_AppendToClassName(this, '" + Control.DynamicHoverStyle.RegisteredCssClass + "')");
+                        writer.AddAttribute("onmouseout", "WebForm_RemoveClassName(this, '" + Control.DynamicHoverStyle.RegisteredCssClass + "')");
+                        writer.AddAttribute("onclick", "IZWebFileManager_HideElement('" + submenuClientId + "')");
+                        writer.RenderBeginTag(HtmlTextWriterTag.Div);
                         var childItem = item.ChildItems[i];
                         ((MenuItemTemplateContainer)_container.GetValue(childItem, null)).RenderControl(writer);
+                        writer.RenderEndTag();
                     }
+                    
                     writer.RenderEndTag();
                     writer.RenderEndTag();
                     writer.RenderEndTag();

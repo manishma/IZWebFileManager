95b5406ef4d9f6c032cfaebbd593cfaff690e9b4
Igor Zelmanovich <igor.israel@gmail.com>
2010-10-22 10:16:05 +0200

    add view state helper methods

---
+++ IZWebFileManager/Components/ViewStateExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.UI;
+
+namespace IZ.WebFileManager.Components
+{
+    static class ViewStateExtensions
+    {
+        public static T GetValue<T>(this StateBag stateBag, string key, T defaultValue)
+        {
+            return (T) (stateBag[key] ?? defaultValue);
+        }
+
+        public static void SetValue<T>(this StateBag stateBag, string key, T value)
+        {
+            stateBag[key] = value;
+        }
+    }
+}

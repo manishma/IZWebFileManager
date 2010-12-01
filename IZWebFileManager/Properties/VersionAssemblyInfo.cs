53adb2716a5de78fee4cde581f3eb8065e99f449
Igor Zelmanovich <igor.israel@gmail.com>
2010-12-01 18:44:19 +0200

    increment version to 2.6.3

--- IZWebFileManager/Properties/VersionAssemblyInfo.cs
+++ IZWebFileManager/Properties/VersionAssemblyInfo.cs
@@ -10,5 +10,5 @@ using System.Reflection;
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: AssemblyVersionAttribute("2.6.2.0")]
+[assembly: AssemblyVersionAttribute("2.6.3.0")]
 

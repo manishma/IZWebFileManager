29569bbaca624860f162100de8a6c42dee02e017
Igor Zelmanovich <igor.israel@gmail.com>
2011-07-21 11:00:10 +0300

    change root directory on post back demo

--- Demo/RootDirectories.aspx
+++ Demo/RootDirectories.aspx
@@ -2,6 +2,14 @@
 <%@ Register Assembly="IZ.WebFileManager" Namespace="IZ.WebFileManager" TagPrefix="iz" %>
 
 <asp:Content ID="Content1" ContentPlaceHolderID="ContentPlaceHolder1" runat="Server">
+    <div style="padding: 10px;">
+        Select directory to view:
+        <asp:DropDownList ID="DropDownList1" runat="server" AutoPostBack="true" OnSelectedIndexChanged="DropDownList1_SelectedIndexChanged">
+            <asp:ListItem Value="/">My Documents</asp:ListItem>
+            <asp:ListItem Value="/My Pictures">My Pictures</asp:ListItem>
+            <asp:ListItem Value="/_hidden">_hidden</asp:ListItem>
+        </asp:DropDownList>
+    </div>
 	<div>
 		<iz:filemanager id="FileManager1" runat="server" height="400" width="600">
         </iz:filemanager>
--- Demo/RootDirectories.aspx.cs
+++ Demo/RootDirectories.aspx.cs
@@ -13,12 +13,27 @@ using IZ.WebFileManager;
 public partial class RootDirectories : System.Web.UI.Page
 {
 	protected void Page_Load (object sender, EventArgs e) {
-
-		FileManager1.RootDirectories.Clear ();
-		RootDirectory rootDirectory = new RootDirectory ();
-		rootDirectory.DirectoryPath = DirectoryManager.GetRootDirectoryPath (Context);
-		rootDirectory.Text = "My Documents";
-		FileManager1.RootDirectories.Add (rootDirectory);
+        if (!IsPostBack)
+        {
+            FileManager1.RootDirectories.Clear();
+            RootDirectory rootDirectory = new RootDirectory();
+            rootDirectory.DirectoryPath = DirectoryManager.GetRootDirectoryPath(Context);
+            rootDirectory.Text = "My Documents";
+            FileManager1.RootDirectories.Add(rootDirectory);
+        }
 
 	}
+
+    protected void DropDownList1_SelectedIndexChanged(object sender, EventArgs e)
+    {
+
+        FileManager1.RootDirectories.Clear();
+        RootDirectory rootDirectory = new RootDirectory();
+        rootDirectory.DirectoryPath = DirectoryManager.GetRootDirectoryPath(Context) + DropDownList1.SelectedValue;
+        rootDirectory.Text = DropDownList1.SelectedItem.Text;
+        FileManager1.RootDirectories.Add(rootDirectory);
+
+        FileManager1.Directory = null;
+        
+    }
 }

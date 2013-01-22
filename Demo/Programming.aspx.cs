7fa5367e6d5ee05405ba013b6ba55557b3365b6d
Igor Zelmanovich <igor.israel@gmail.com>
2013-01-22 11:49:41 +0200

    upgrade demo project

--- Demo/Programming.aspx.cs
+++ Demo/Programming.aspx.cs
@@ -27,6 +27,37 @@ public partial class Programming : System.Web.UI.Page
 		rootDirectory.Text = "My Documents";
 		fileManager.RootDirectories.Add (rootDirectory);
 
+        fileManager.ExecuteCommand += FileManagerOnExecuteCommand;
+
+        FileManagerCommand wyswygCmd = new FileManagerCommand();
+        wyswygCmd.CommandName = "WYSWYG";
+        wyswygCmd.Name = "Edit with WYSWYG editor";
+
+        FileManagerCommand editCmd = new FileManagerCommand();
+        editCmd.CommandName = "EditText";
+        editCmd.Name = "Edit";
+
+	    FileType htmlFileType = new FileType();
+        htmlFileType.Extensions = "htm, html";
+        htmlFileType.Name = "HTML Document";
+        htmlFileType.LargeImageUrl = "images/32x32/html.gif";
+        htmlFileType.SmallImageUrl = "images/16x16/html.gif";
+        htmlFileType.Commands.Add(wyswygCmd);
+        htmlFileType.Commands.Add(editCmd);
+        fileManager.FileTypes.Add(htmlFileType);
+
+        FileType txtFileType = new FileType();
+        txtFileType.Extensions = "txt, js, css";
+        txtFileType.Name = "HTML Document";
+        txtFileType.Commands.Add(editCmd);
+        fileManager.FileTypes.Add(txtFileType);
+
 		PlaceHolder1.Controls.Add (fileManager);
 	}
+
+    private void FileManagerOnExecuteCommand(object sender, ExecuteCommandEventArgs e)
+    {
+        e.ClientScript = "alert('Use ExecuteCommand event to handle your custom command.\\nCommandName=" + e.CommandName +
+                         "\\nItem=" + e.Items[0].VirtualPath.Replace("'", "\\'") + "')";
+    }
 }

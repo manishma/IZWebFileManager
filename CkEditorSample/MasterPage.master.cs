c2f908fab6e53f188b14cb8a135915eb6a2b7db7
Bruno Migliaretti <info@magicbusmultimedia.it>
2014-10-12 15:40:21 +0200

    Add setting of user privileges

--- CkEditorSample/MasterPage.master.cs
+++ CkEditorSample/MasterPage.master.cs
@@ -4,11 +4,12 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using MB.FileBrowser
 
 public partial class MasterPage : System.Web.UI.MasterPage
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-
+        MagicSession.Current.ReadOnly = true;
     }
 }
